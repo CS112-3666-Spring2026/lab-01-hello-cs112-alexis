b0a770e77f8a376414d24e3bd7e043c326bbc382
AlexisZ-CS <alexiszamudiog2001@gmail.com>
2026-05-09 18:26:28 +0000

    finished part 2

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS 112!");
+        System.out.println("Do I like food? " + Main.isAlike("food"));
+        System.out.println("Do I like fishing? " + Main.isAlike("faking"));
+        Main.printAboutMe();
+
+    }
+
+
+    public static void printAboutMe(){
+        System.out.println("I like food, gym and playing video games with friends");
+    }
+    
+    public static boolean isAlike(String topic) {
+        String myLike = "I like food, gym and playing video games with friends";
+
+        return myLike.contains(topic);
     }
 }
